e903d470ea88a1df5c5d49c4d4590db073ef5244
EC2 Default User <ec2-user@ip-172-31-7-55.ca-central-1.compute.internal>
2021-06-03 07:51:46 +0000

    mp

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const axios = require('axios');
-const URL = `https://sattjp.webhook.office.com/webhookb2/b65fd0a7-58a0-4f96-b109-07ba0f9b6ad6@8c4fbddb-31dd-42ec-b709-917ff3144a77/IncomingWebhook/06911777bba44ccdb90596b862362dfa/7d3fc861-8da4-4895-a426-86a566ed1598`; //各自のWebhook URL
 
 // for (var i = 0; i < process.argv.length; i++) {
 //   console.log("argv[" + i + "] = " + process.argv[i]);
@@ -12,9 +11,10 @@ const core = require('@actions/core');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const ami = core.getInput('ami');
-  console.log(`${ami} is error`);
-  axios.post(URL, { text: `${ami} is error` })
+  const AMI = core.getInput('ami');
+  const URL = core.getInput('webhook'); //各自のWebhook URL
+  console.log(`${AMI} is error`);
+  axios.post(URL, { text: `${AMI} is error` })
     .then(res => console.log(res.data));
   // const time = (new Date()).toTimeString();
   // core.setOutput("time", time);
