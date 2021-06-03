567ecf06de61660805ffba67d8a5b73d25fb3e3a
EC2 Default User <ec2-user@ip-172-31-7-55.ca-central-1.compute.internal>
2021-06-03 05:34:05 +0000

    s

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const axios = require('axios');
 const URL = `https://sattjp.webhook.office.com/webhookb2/b65fd0a7-58a0-4f96-b109-07ba0f9b6ad6@8c4fbddb-31dd-42ec-b709-917ff3144a77/IncomingWebhook/06911777bba44ccdb90596b862362dfa/7d3fc861-8da4-4895-a426-86a566ed1598`; //各自のWebhook URL
 
-axios.post(URL, { text: 'テスト' })
+axios.post(URL, { text: 'V2テスト' })
   .then(res => console.log(res.data));
 
 for (var i = 0; i < process.argv.length; i++) {
